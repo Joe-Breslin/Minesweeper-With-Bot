1363c9fae3b565553d4d8aaea3942099ba4bdb34
Joe Breslin <joebreslin888@gmail.com>
2026-08-01 17:34:12 -0600

    Added user_inp bomb %

--- main.py
+++ main.py
@@ -24,7 +24,22 @@ def main():
         except ValueError:
             print("Please enter a valid whole number")
 
+    while True: 
+
+        bomb_pct = input(
+            "Enter the percentage of the board that will be bombs [Typical value is 0.15] :"
+            )
+        try:
+            bomb_pct = float(bomb_pct)
+            if 0 < bomb_pct < 1:
+                break
+            else:
+                print("Please enter a value between 0 and 1")
+
+        except ValueError:
+            print("Please enter a valid value between 0 - 1")
+
     root = tk.Tk()
-    app = GUI.MinesweeperGUI(root, user_width, user_height, cell_size=40)
+    app = GUI.MinesweeperGUI(root, user_width, user_height, bomb_pct, cell_size=40)
     root.mainloop()
 main()
